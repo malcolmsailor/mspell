2693b2fc23692512e0857b8b69dea4bf7f13a779
Malcolm Sailor <malcolm.sailor@gmail.com>
2021-09-10 15:18:29 -0400

    update __init__.py

--- mspell/__init__.py
+++ mspell/__init__.py
@@ -1,22 +1,3 @@
 from .group_speller import GroupSpeller
 from .speller import Speller
 from .unspeller import Unspeller
-
-# TODO commit to branch, remove
-# from .chords import (
-#     AbstractRoot,
-#     ChordReader,
-#     Figure,
-#     FigureReader,
-#     Quality,
-#     QualityReader,
-#     LabeledChord,
-#     Root,
-#     RootReader,
-#     RNReader,
-#     RNRoot,
-#     RNRootReader,
-#     RNQualityReader,
-# )
-
-# from .key import Key
